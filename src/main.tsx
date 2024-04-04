80f2789f620b81fea92a04829893bb5d4036a0ec
Harsh Tejani <tejani317@gmail.com>
2024-04-04 17:10:41 -0400

    implemented useQuery for data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import router from "./router/route.tsx";
 import { RouterProvider } from "react-router-dom";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,74 +1,89 @@
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [response, setResponse] = useState();
-  useEffect(() => {
-    getApiDate();
-  }, []);
-
-  async function getApiDate() {
-    await fetch(
-      "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=wKxsqvALK0fsmlLKwiJbcCMkZJG1vS8erbX3dgqf"
-    )
-      .then((responseData) => responseData.json())
-      .then((response) => setResponse(response.photos));
-  }
-
-  const photos: [
-    {
-      id: number;
-      camera: {
-        id: number;
-        name: string;
-        rover_id: number;
-        full_name: string;
-      };
-      img_src: string;
-      earth_date: Date;
-      rover: {
-        id: number;
-        name: string;
-        landing_date: Date;
-        launch_date: Date;
-        status: string;
-        max_sol: number;
-        max_date: Date;
-        total_photos: number;
-        cameras: [
-          {
-            name: string;
-            full_name: string;
-          },
-          {
-            name: string;
-            full_name: string;
-          },
-          {
-            name: string;
-            full_name: string;
-          },
-          {
-            name: string;
-            full_name: string;
-          },
-          {
-            name: string;
-            full_name: string;
-          },
-          {
-            name: string;
-            full_name: string;
-          },
-          {
-            name: string;
-            full_name: string;
-          }
-        ];
-      };
-    }
-  ] = response?.map((a) => {
-    return a;
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: () =>
+      fetch(
+        "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=wKxsqvALK0fsmlLKwiJbcCMkZJG1vS8erbX3dgqf"
+      ).then((res) => res.json()),
   });
 
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  // const [response, setResponse] = useState();
+  // useEffect(() => {
+  //   getApiDate();
+  // }, []);
+
+  // async function getApiDate() {
+  //   await fetch(
+  //     "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=wKxsqvALK0fsmlLKwiJbcCMkZJG1vS8erbX3dgqf"
+  //   )
+  //     .then((responseData) => responseData.json())
+  //     .then((response) => setResponse(response.photos));
+  // }
+
+  const photos: IPhotos[] =
+    data ??
+    [].map((a) => {
+      return a;
+    });
+
   return <>{JSON.stringify(photos)}</>;
+}
+
+interface IPhotos {
+  id: number;
+  camera: {
+    id: number;
+    name: string;
+    rover_id: number;
+    full_name: string;
+  };
+  img_src: string;
+  earth_date: Date;
+  rover: {
+    id: number;
+    name: string;
+    landing_date: Date;
+    launch_date: Date;
+    status: string;
+    max_sol: number;
+    max_date: Date;
+    total_photos: number;
+    cameras: [
+      {
+        name: string;
+        full_name: string;
+      },
+      {
+        name: string;
+        full_name: string;
+      },
+      {
+        name: string;
+        full_name: string;
+      },
+      {
+        name: string;
+        full_name: string;
+      },
+      {
+        name: string;
+        full_name: string;
+      },
+      {
+        name: string;
+        full_name: string;
+      },
+      {
+        name: string;
+        full_name: string;
+      }
+    ];
+  };
 }
